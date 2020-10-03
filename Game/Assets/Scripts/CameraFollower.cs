ed087bb272196db3035dce831b697b2cab4c45e0
Hendrik Rakemann <hendrik.rakemann@gmx.de>
2020-10-03 17:09:27 +0200

    World Curvature No more Fog Skybox

---
+++ Game/Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField] private Transform m_player;
+    // Update is called once per frame
+    void Update()
+    {
+        var targetPos = this.transform.position;
+        targetPos.z = this.m_player.transform.position.z + 5f;
+        this.transform.position = targetPos;
+    }
+}
