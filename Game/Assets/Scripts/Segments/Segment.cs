5166da374b3e4be0254f58f79a6b97b0cdf7fa0c
Hendrik Rakemann <hendrik.rakemann@gmx.de>
2020-10-03 16:09:50 +0200

    Shadergraph for World Bending Segments implemented Start/End Segment

---
+++ Game/Assets/Scripts/Segments/Segment.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Segments
+{
+    public class Segment : MonoBehaviour
+    {
+        [SerializeField] protected Transform m_end;
+        [SerializeField] protected Transform m_start;
+
+        public Transform End => this.m_end;
+        public Transform Start => this.m_start;
+    }
+}
---
+++ Game/Assets/Scripts/Segments/SpawnSegment.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Segments
+{
+    public class SpawnSegment : Segment
+    {
+        [SerializeField] private Transform m_spawnPoint;
+
+        public Transform SpawnPoint => this.m_spawnPoint;
+    }
+}
