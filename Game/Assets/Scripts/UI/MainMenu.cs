643309e24ee0bb4d51fd5b18d1821238a2a0728c
Melissa Richter <m.richter212@gmx.de>
2020-10-11 11:45:44 +0200

    Implement Title Screen ($127)

---
+++ Game/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{  
+    public void BtnPlay_Click()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void BtnQuit_Click()
+    {
+        Application.Quit();
+    }
+
+    public void BtnHowTo_Click()
+    {
+
+    }
+
+    public void BtnCredits_Click()
+    {
+
+    }
+}
